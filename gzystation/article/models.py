03803edc8d0d8fd45ccdb2c509e71eeda0d0138d
Ender451 <710228838@qq.com>
2017-10-10 00:14:12 +0800

    归档 分页

---
+++ gzystation/article/models.py
@@ -0,0 +1,103 @@
+# coding:utf-8
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+import markdown
+from django.utils.html import strip_tags
+# Create your models here.
+
+
+class Category(models.Model):
+    #分类名称
+    name = models.CharField(max_length=50,verbose_name="分类名称")
+
+    class Meta:
+        verbose_name = '分类名称'
+        verbose_name_plural = '分类名称'
+
+    def __str__(self):
+        return self.name
+    
+    def get_article_num(self):
+        num= Category.objects.filter(article__category__name=self.name).count()
+        return num
+
+class Tag(models.Model):
+    #名称
+    name = models.CharField(max_length=50,verbose_name="标签名称")
+
+    #admin界面中Tag项目显示的名称
+    class Meta:
+        verbose_name = '标签名称'
+        verbose_name_plural = '标签名称'
+    
+    #admin界面中每个项目显示自己的name
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    #文章标题
+    title = models.CharField(max_length=70,verbose_name="文章标题")
+    #文章内容
+    body = models.TextField(verbose_name="文章内容",default='')
+    #创建时间
+    create_time = models.DateTimeField(verbose_name="创建时间")
+    #修改时间
+    modified_time = models.DateTimeField(verbose_name="修改时间")
+    #摘要
+    excerpt = models.CharField(max_length=200,blank=True,verbose_name="摘要")
+    #分类
+    category = models.ForeignKey(Category,verbose_name="分类")
+    #标签
+    tags = models.ManyToManyField(Tag,blank=True,verbose_name=u"标签")
+    #作者
+    author = models.ForeignKey(User,verbose_name="作者")
+    #浏览量
+    views = models.PositiveIntegerField(default=0, verbose_name="浏览量")
+
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+        #默认排序
+        ordering=['-create_time']
+
+    #自动生成摘要
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            
+            md= markdown.Markdown(
+                extensions=[
+                           'markdown.extensions.extra',
+                           'markdown.extensions.codehilite',
+                           ]
+                )
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Article,self).save(*args,**kwargs)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+    #定义 get_absolute_url 方法 获取自身的url 
+    def get_absolute_url(self):
+        # urls.py   中 request 传入的是 id
+        # views.py  中 get_object_or_404()函数传入 pk 参数 id
+        return reverse('article:detail',kwargs={'id':self.pk})
+        # article --> article/urls.py app_name=article
+        # detail  --> article/urls.py name='detail'
+
+    #统计浏览量
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+
+
+
+
---
+++ gzystation/article/views.py
@@ -0,0 +1,446 @@
+from django.shortcuts import render,get_object_or_404
+from article.models import Article,Category,Tag
+import markdown
+# Create your views here.
+
+from django.http import HttpResponse
+import pygments
+from comments.forms import CommentForm
+
+from django.views.generic import ListView,DetailView
+
+def articlelist(request):
+    articles = Article.objects.all()[:10]
+    context = {'articles':articles}
+    return render(request,'article/articlelist.html',context)
+
+#与首页 文章list 代码一致 可以精简
+class ArticlelistView(ListView):
+    model= Article
+    template_name = 'article/articlelist.html'
+    context_object_name = 'articles'
+
+    #分页功能(重复代码)
+    paginate_by = 7
+
+    def get_context_data(self,**kwargs):
+        context =super().get_context_data(**kwargs)
+
+        paginator= context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated= context.get('is_paginated')
+        #print(paginator,page,is_paginated)
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+
+        #print(pagination_data)
+        context.update(pagination_data)
+
+        return context
+
+    def pagination_data(self,paginator,page,is_paginated):
+        if not is_paginated:
+
+             return {}
+
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+
+        page_range = paginator.page_range
+
+        if page_number == 1 :
+            right = page_range[page_number:page_number +2 ]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number -3 ) if (page_number -3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_number - 3) if (page_number -3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+               right_has_more = True
+            if right[-1] < total_pages:
+               last = True
+
+            if left[0] > 2:
+               left_has_more = True
+            if left[0] > 1:
+               first = True
+
+        data = {
+            'left':left,
+            'right': right,
+            'left_has_more':left_has_more,
+            'right_has_more':right_has_more,
+            'first':first,
+            'last':last,
+             }
+        print(data)
+        return data
+
+def detail(request,id):
+    #pk primary_key 主键
+    article= get_object_or_404(Article,pk=id)
+
+    #阅读量+1
+    article.increase_views()
+
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         'markdown.extensions.extra',
+                                         'markdown.extensions.codehilite',
+                                         'markdown.extensions.toc',
+                                               ]
+                                    )
+    form = CommentForm()
+    comment_list = article.comment_set.all()
+
+    context={
+             'article':article,
+             'form':form,
+             'comment_list':comment_list
+            }
+    return render(request,'article/article.html',context=context)
+
+#文章 基于类
+class ArticleDetailView(DetailView):
+    model= Article
+    template_name = 'article/article.html'
+    context_object_name = 'article'
+
+    def get(self,request,*args,**kwargs):
+        response= super(ArticleDetailView,self).get(request,*args,**kwargs)
+        self.object.increase_view()
+        return response
+
+    def get_object(self,queryset=None):
+        article = super(ArticleDetailView,self).get_object(queryset=None)
+        article.body = markdown.markdown(
+                                        article.body,
+                                        extensions=[
+                                            'markdown.extensions.extra',
+                                            'markdown.extensions.codehilite',
+                                            'markdown.extensions.toc',
+                                                   ]
+                                        )       
+        return article
+
+    def get_context_data(self,**kwargs):
+        context = super(ArticleDetailView,self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+                      'form':form,
+                      'comment_list':comment_list
+                      })
+        return context
+
+
+
+#归档 视图
+def archives(request,year,month):
+    print(year,month)
+    articles = Article.objects.filter(create_time__year=year,
+                                      create_time__month=month
+                                   ).order_by('-create_time')
+    #print(articles)
+    context = {'articles':articles}
+    return render(request,'article/articlelist.html',context)
+
+#归档 基于类
+class Archivesview(ListView):
+    model = Article
+    template_name = 'article/articlelist.html'
+    context_object_name = 'articles'
+
+    #分页功能(重复代码)
+    #归档这里的分页需要修改，这里django自带的分页是以model：Article为对象进行的分页，并没有对年月进行过滤处理。
+    #但是在context 传参的时候 我这里是进行过年月过滤处理的文章，所以分页与文章列表不一致，需要修改
+    paginate_by = 7
+    
+    def get_context_data(self,**kwargs):
+        context =super().get_context_data(**kwargs)
+
+        paginator= context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated= context.get('is_paginated')
+        print(paginator,page,is_paginated)
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+
+        print('归档',pagination_data)
+        context.update(pagination_data)
+     
+        year=self.kwargs.get('year')
+        month=self.kwargs.get('month')
+        print('year:',year)
+        print('month:',month)
+        articles = Article.objects.filter(create_time__year=year,
+                                      create_time__month=month
+                                   ).order_by('-create_time')
+        context['articles'] = articles
+        #print(context)
+        return context
+
+    def pagination_data(self,paginator,page,is_paginated):
+        if not is_paginated:
+
+             return {}
+
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+
+        page_range = paginator.page_range
+
+        if page_number == 1 :
+            right = page_range[page_number:page_number +2 ]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number -3 ) if (page_number -3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_number - 3) if (page_number -3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+               right_has_more = True
+            if right[-1] < total_pages:
+               last = True
+
+            if left[0] > 2:
+               left_has_more = True
+            if left[0] > 1:
+               first = True
+
+        data = {
+            'left':left,
+            'right': right,
+            'left_has_more':left_has_more,
+            'right_has_more':right_has_more,
+            'first':first,
+            'last':last,
+             }
+        print(data)
+        return data
+        
+
+
+#分类 视图
+def category(request,pk):
+    cate = get_object_or_404(Category,pk=pk)
+    articles = Article.objects.filter(category=cate).order_by('-create_time')
+    context={'articles':articles,'category':cate}
+    return render(request,'article/article_category_list.html',context)
+
+#分类 基于类
+class CategoryView(ListView):
+    model = Article
+    template_name = 'article/article_category_list.html'
+    context_object_name = 'articles'
+
+    paginate_by = 5
+   
+    #重写父类的 get_queryset() 方法
+    def get_queryset(self):
+        cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        #print('cate',cate)
+        return super(CategoryView,self).get_queryset().filter(category=cate)
+
+
+    def get_context_data(self,**kwargs):
+        context =super().get_context_data(**kwargs)
+
+        paginator= context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated= context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+
+        print('分类',pagination_data)
+        context.update(pagination_data)
+        name= get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        #print('name:',name)
+        context['categoryname'] = name
+        #print(context)
+        return context
+
+    #分页功能（重复代码）
+    def pagination_data(self,paginator,page,is_paginated):
+        if not is_paginated:
+
+             return {}
+
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+
+        page_range = paginator.page_range
+
+        if page_number == 1 :
+            right = page_range[page_number:page_number +2 ]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number -3 ) if (page_number -3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_number - 3) if (page_number -3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+               right_has_more = True
+            if right[-1] < total_pages:
+               last = True
+
+            if left[0] > 2:
+               left_has_more = True
+            if left[0] > 1:
+               first = True
+
+        data = {
+            'left':left,
+            'right': right,
+            'left_has_more':left_has_more,
+            'right_has_more':right_has_more,
+            'first':first,
+            'last':last,
+             }
+        return data
+
+#标签 基于类
+class TagView(ListView):
+    model = Article
+    template_name = 'article/article_tag_list.html'
+    context_object_name = 'articles'
+    paginate_by = 5
+
+    #重写父类的 get_queryset() 方法
+    def get_queryset(self):
+        cate = get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        #print('tag',cate)
+        return super(TagView,self).get_queryset().filter(tags=cate)
+
+
+    def get_context_data(self,**kwargs):
+        context =super().get_context_data(**kwargs)
+
+        paginator= context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated= context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator,page,is_paginated)
+
+        context.update(pagination_data)
+        name= get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        print('tagname:',name)
+        context['tagname'] = name
+
+        return context
+
+    #分页功能（重复代码）
+    def pagination_data(self,paginator,page,is_paginated):
+        if not is_paginated:
+
+             return {}
+
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+
+        page_range = paginator.page_range
+
+        if page_number == 1 :
+            right = page_range[page_number:page_number +2 ]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number -3 ) if (page_number -3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_number - 3) if (page_number -3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+               right_has_more = True
+            if right[-1] < total_pages:
+               last = True
+
+            if left[0] > 2:
+               left_has_more = True
+            if left[0] > 1:
+               first = True
+
+        data = {
+            'left':left,
+            'right': right,
+            'left_has_more':left_has_more,
+            'right_has_more':right_has_more,
+            'first':first,
+            'last':last,
+             }
+        return data
