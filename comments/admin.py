ab6c279a371595092caac28feb83ec8da6390636
Ender451 <710228838@qq.com>
2017-10-14 21:11:48 +0800

    增加评论后台管理

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from comments.models import Comment
 # Register your models here.
+
+
+class CommentAdmin(admin.ModelAdmin):
+    #列表展示的字段信息
+    list_display = ('name','email','text','create_time','article','independentarticle',)
+    #搜索索引
+    search_fields = ('name','text',)
+
+
+
+
+admin.site.register(Comment,CommentAdmin)
+
+
--- comments/models.py
+++ comments/models.py
@@ -3,15 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Comment(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=100,verbose_name="评论者")
+    email = models.EmailField(max_length=255,verbose_name="Email地址")
     url = models.URLField(blank=True)
-    text = models.TextField()
-    create_time = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name="评论")
+    create_time = models.DateTimeField(auto_now_add=True,verbose_name="评论时间")
 
-    article= models.ForeignKey('article.Article',blank=True,null=True)
+    article= models.ForeignKey('article.Article',blank=True,null=True,verbose_name="所属文章")
     #独立文章 与 一般文章 共用一套评论，所以增加一个外键
-    independentarticle= models.ForeignKey('independentarticle.Independentarticle',blank=True,null=True)
+    independentarticle= models.ForeignKey('independentarticle.Independentarticle',blank=True,null=True,verbose_name="所属独立文章")
+
+    class Meta:
+        verbose_name = '评论'
+        verbose_name_plural = '所有评论'
+
  
     def __str__(self):
         return self.text[:20]
